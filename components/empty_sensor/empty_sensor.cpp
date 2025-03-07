7604169a06918a45ed5d692d0e15e6b981b87841
Schmurtz <schmurtz@gmail.com>
2025-03-07 01:20:15 +0100

    update test

---
+++ components/empty_sensor/empty_sensor.cpp
@@ -0,0 +1,18 @@
+#include "empty_sensor.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace empty_sensor {
+
+
+
+void EmptySensor::setup() {}
+
+void EmptySensor::loop() {}
+
+void EmptySensor::update() {}
+
+void EmptySensor::dump_config() { }
+
+}  // namespace empty_sensor
+}  // namespace esphome
--- components/powersensor/sensor.py
+++ components/powersensor/sensor.py
@@ -4,9 +4,9 @@ from esphome.components import sensor
 from esphome.const import UNIT_WATT, DEVICE_CLASS_ENERGY, STATE_CLASS_MEASUREMENT
 
 powersensor_ns = cg.esphome_ns.namespace("powersensor")
-MyPowerSensor = powersensor_ns.class_("MyPowerSensor", cg.PollingComponent)
+MyPowerSensor = powersensor_ns.class_("MyPowerSensor", sensor.Sensor, cg.PollingComponent)
 
-CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = sensor.sensor_schema({
     cv.GenerateID(): cv.declare_id(MyPowerSensor),
     cv.Optional("realpower_sensor1"): sensor.sensor_schema(),
     cv.Optional("realpower_sensor2"): sensor.sensor_schema(),
@@ -19,18 +19,17 @@ async def to_code(config):
     await cg.register_component(var, config)
 
     if "realpower_sensor1" in config:
-        var.realpower_sensor1 = await sensor.new_sensor(config["realpower_sensor1"])
-        cg.add(var.realpower_sensor1)
+        realpower_sensor1 = await sensor.new_sensor(config["realpower_sensor1"])
+        cg.add(var.set_sensor1(realpower_sensor1))
 
     if "realpower_sensor2" in config:
-        var.realpower_sensor2 = await sensor.new_sensor(config["realpower_sensor2"])
-        cg.add(var.realpower_sensor2)
+        realpower_sensor2 = await sensor.new_sensor(config["realpower_sensor2"])
+        cg.add(var.set_sensor2(realpower_sensor2))
 
     if "realpower_sensor3" in config:
-        var.realpower_sensor3 = await sensor.new_sensor(config["realpower_sensor3"])
-        cg.add(var.realpower_sensor3)
+        realpower_sensor3 = await sensor.new_sensor(config["realpower_sensor3"])
+        cg.add(var.set_sensor3(realpower_sensor3))
 
     if "realpower_sensor4" in config:
-        var.realpower_sensor4 = await sensor.new_sensor(config["realpower_sensor4"])
-        cg.add(var.realpower_sensor4)
-
+        realpower_sensor4 = await sensor.new_sensor(config["realpower_sensor4"])
+        cg.add(var.set_sensor4(realpower_sensor4))
